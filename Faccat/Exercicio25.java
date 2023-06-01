d7c708b763a4c1c90f15cd02879cc7bb5bcf0e29
LeilianeR <134979355+LeilianeR@users.noreply.github.com>
2023-05-31 22:22:09 -0300

    Add files via upload

--- Faccat/Exercicio25.java
+++ Faccat/Exercicio25.java
@@ -1,10 +1,39 @@
 package exerciciosFaccat;
 
+
+import java.util.Scanner;
 public class Exercicio25 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 try {
+			 double conta, saldo=0, saldoAtual, debito=0, credito=0;
+				
+				Scanner read= new Scanner(System.in);
+				System.out.println("Digite o número da conta: ");
+				conta=read.nextDouble();
+				System.out.println("Saldo: ");
+				saldo= read.nextDouble();
+				System.out.println("Débito: ");
+				debito=read.nextDouble();
+				System.out.println("Credito: ");
+				credito=read.nextDouble();
+				
+				saldoAtual= saldo-debito+credito;
+				if (saldoAtual>=0) {
+					System.out.println("Saldo positivo! :)");
+				} else {
+					System.out.println("Saldo negativo! :(");
 
+				}
+			
+		} catch (Exception e) {
+			System.out.println("Opção inválida! ");
+		}
+			
+		}
+		
+		
+		
 	}
 
-}
+
